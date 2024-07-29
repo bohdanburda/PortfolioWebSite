b112a4b936864d1f25a1372b9375c81311f26070
Bohdan Burda <b.burda2002@gmail.com>
2024-07-29 20:14:35 +0300

    mini update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import Technologies from './components/technologies/technologies'
-import Experience from './components/experience/Experience'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/footer'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Technologies from "./components/technologies/technologies";
+import Experience from "./components/experience/Experience";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/footer";
 
 const App = () => {
   return (
-    <div className='bg_section'>
-        <Header />
-        <Nav />
-        <Technologies />
-        <Experience />
-        <Contact />
-        <Footer />
+    <div className="bg_section">
+      <Header />
+      <Nav />
+      <Experience />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+//<Technologies />
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import ReactDOM from "react-dom";
-import App from './App';
-import './index.css'
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector("#root"));
 
-const [red, green, blue] = [0, 0, 0]
-const section1 = document.querySelector('.bg_section')
+const [red, green, blue] = [0, 0, 0];
+const section1 = document.querySelector(".bg_section");
 
-window.addEventListener('scroll', () => {
-  const y = 1 + (window.scrollY || window.pageYOffset)
-  const [r, g, b] = [red, green, blue + y/5 > 149 ? 149 : blue + y/5].map(Math.round)
-  section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-})
+window.addEventListener("scroll", () => {
+  const y = 1 + (window.scrollY || window.pageYOffset);
+  // #FFB200
+  const [r, g, b] = [
+    red + y / 5 > 255 ? 255 : red + y / 5,
+    green + y / 7 > 178 ? 178 : green + y / 7,
+    0,
+  ].map(Math.round);
+  section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
